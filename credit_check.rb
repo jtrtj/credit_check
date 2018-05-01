9e4b58dd43ffe5a848838dbdfa651f286cf9d3f8
JohnRoemer <jt.r@live.com>
2018-04-30 21:22:16 -0600

    Deleted credit_check.rb from main dir.

--- credit_check.rb
+++
@@ -1,9 +0,0 @@
-card_number = "4929735477250543"
-
-valid = false
-
-# Your Luhn Algorithm Here
-
-# Output
-## If it is valid, print "The number is valid!"
-## If it is invalid, print "The number is invalid!"
